6b03e51af4652ba98dbe4e696dfcab905113b5a1
zhangwl <zhangwl202322@163.com>
2024-09-21 12:21:59 +0800

    1. 数据处理，筛选正弦波，提取峰值

---
+++ temperature_fitting.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_excel('附件一（训练集）.xlsx')
+filtered_df = df[df['励磁波形'] == '正弦波']
+first_part = filtered_df.iloc[:, :4]
+second_part = filtered_df.iloc[:, 4:]
+max_values = second_part.max(axis=1)
+third_part = pd.DataFrame(max_values, columns=['峰值'])
+new_first_part = pd.concat([first_part, third_part], axis=1)
